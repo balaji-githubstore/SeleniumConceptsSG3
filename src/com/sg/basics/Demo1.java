5a0dc9e2e8a8c6aac574ddb1f5071990ed983b68
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-07-14 17:14:49 +0530

    added comment

--- src/com/sg/basics/Demo1.java
+++ src/com/sg/basics/Demo1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+//first program
 public class Demo1 {
 
 	public static void main(String[] args) {
